cd51432432c25772ef359674baf2f4ab4a9ab78d
johninsan <johninsan@gmail.com>
2022-11-16 11:22:41 +0700

    profloss laba kotor

--- app/Http/Controllers/ProfitLossController.php
+++ app/Http/Controllers/ProfitLossController.php
@@ -32,16 +32,32 @@ class ProfitLossController extends Controller
         }
         $data_hapok = array(
             'Nama' => 'Harga Pokok Penjualan',
-            'total_penjualan' => $sum_hapok,
+            'total_hapok' => $sum_hapok,
         );
         return $data_hapok;
+    }
+
+    public function potongan_beli()
+    {
+        $sum_potong_beli = 0;
+        $potong_beli = Jurnal::where('Chart_Of_Account', 'Potongan Pembelian')->get();
+        foreach ($potong_beli as $x) {
+            $ending_balance = $x->ending_balance;
+            $sum_potong_beli += $ending_balance;
+        }
+        $data_potong_beli = array(
+            'Nama' => 'Potongan Pembelian',
+            'total_pot_beli' => $sum_potong_beli,
+        );
+        return $data_potong_beli;
     }
 
     public function prof_loss()
     {
         $penjualan = $this->penjualan();
         $hapok = $this->hp_penjualan();
-        dd($hapok);
+        $potong_beli = $this->potongan_beli();
+        $laba_kotor = (($penjualan['total_penjualan'] + $hapok['total_hapok']) - $potong_beli['total_pot_beli']);
         return view('jurnal.profloss.profloss', compact('penjualan'));
     }
 }

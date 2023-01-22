89653320c9e02cb1c34dbbb8abf49d7ac1af58f4
Fathurohmanoz <fathurfurs@gmail.com>
2023-01-22 12:27:40 +0700

    print proses 2

--- resources/views/pdf/order_pdf.blade.php
+++ resources/views/pdf/order_pdf.blade.php
@@ -6,104 +6,109 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    {{-- <link type="text/css" href="{{ public_path('argon') }}/css/invoice.css" rel="stylesheet"> --}}
-    <link href="{{ asset('argon') }}/img/brand/favicondevifo.png" rel="icon" type="image/png">
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
-    <!-- Extra details for Live View on GitHub Pages -->
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
-    <!-- Icons -->
-    <link href="{{ asset('argon') }}/vendor/nucleo/css/nucleo.css" rel="stylesheet">
-    <link href="{{ asset('argon') }}/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
-    <!-- Argon CSS -->
-    <link type="text/css" href="{{ asset('argon') }}/css/argon.css?v=1.0.0" rel="stylesheet">
-    <link type="text/css" href="{{ asset('argon') }}/css/my.css" rel="stylesheet">
+    <link type="text/css" href="{{ public_path('argon') }}/css/nota.css" rel="stylesheet">
+
 </head>
 
 <body>
-    <div class="container-fluid mt--7">
-        <div class="row">
-            <div class="col">
-                <div class="card shadow">
-                    <div class="card-header border-0">
-                        <div class="row align-items-center">
-                            <div class="col-8">
-                                <h3 class="mb-0"> <a href="{{ route('pos.index') }}" type="button"
-                                    class="btn btn-sm btn-info">{{ __('Back') }}</a> Order</h3>
-                                <p>Kode : <b>{{ $data['order']->kode_nota}}</b> <br>
-                                    Nama : <b>{{ $data['order']->nama_pembeli}}</b> <br>
-                                    No.HP : <b>{{ $data['order']->no_pembeli}}</b>
-                                </p>
+    <div  class="table-header">
+        <table style="table-layout:fixed;">
+            <thead>
+                <tr>
+                    <th>1</th>
+                    <th>2</th>
 
-                            </div>
-                            <div class="col-4 text-right">
-
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div class="mx-3">
-                        <div class="table-responsive">
-                            <table id="myTable" class="table align-items-center table-flush">
-                                <thead class="thead-light">
+                </tr>
+            </thead>
+            <tbody>
+                    <tr>
+                        <td style="text-align: left">
+                            <table style="text-align: left">
+                                <tr>
+                                    <td colspan="3" style="text-align: left;">
+                                        <img style="width: 80%" src="{{ public_path('argon') }}/img/devifo.jpg">
+                                    </td>
+                                </tr>
+                                <tbody>
                                     <tr>
-                                        <th scope="col">Kode</th>
-                                        <th scope="col">Nama</th>
-                                        <th scope="col">Harga</th>
-                                        <th scope="col">Jumlah</th>
-                                        <th scope="col">Total</th>
-                                        <th class="text-center" scope="col">Action</th>
-                                        {{-- <th scope="col">Delete</th> --}}
+                                        <td colspan="3" style="text-align: left;font-size: 9pt">
+                                            Jl. Cibanteng Agathis, Cihideung Ilir, Kec. Ciampea, Kabupaten Bogor, Jawa Barat 16620
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="3" style="text-align: center;font-size: 9pt">
+                                            Telp.0813-7774-7001
+                                        </td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                        </td>
+
+                        <td style="text-align: right">
+                            <table style="text-align: right">
+                                <tr>
+                                    <td colspan="3" style="text-align: right;">Bogor, {{ Carbon\Carbon::now()->format('d F Y')}}</td>
+                                </tr>
+                                <tbody>
+                                    <tr>
+                                        <td colspan="3" style="text-align: right;height: 50px;">Kepada Yth.</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 20%">Nama</td>
+                                        <td style="width: 1%">:</td>
+                                        <td style="width: 10%"><b>{{ $data['order']->nama_pembeli}}</b></td>
+                                    </tr>
+                                    <tr>
+                                        <td>No.HP</td>
+                                        <td>:</td>
+                                        <td><b>{{ $data['order']->no_pembeli}}</b></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+
+                    </tr>
+            </tbody>
+        </table>
+    </div><br>
+    NOTA No : <b>{{ $data['order']->kode_nota}}</b>
+                        <div  class="table-request">
+                            <table border="1" style="table-layout:fixed;">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 5%;height: 30px;">No</th>
+                                        <th style="width: 20%;">Kode</th>
+                                        <th style="width: 20%;">Nama</th>
+                                        <th style="width: 20%;">Harga</th>
+                                        <th style="width: 20%;">Jumlah</th>
+                                        <th style="width: 50%;">Total</th>
+
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @foreach ($data['data'] as $x)
+                                    @foreach ($data['data'] as $index=>$x)
                                         <tr>
-                                            <td> {{ $x->barangs->kode_barang }} </td>
+                                            <td>{{ $index+1 }}</td>
+                                            <td style="height: 30px"> {{ $x->barangs->kode_barang }} </td>
                                             <td>{{ $x->barangs->nama_barang }} </td>
-                                            <td> Rp. {{ number_format((float) $x->barangs->harga_barang) }}</td>
+                                            <td> Rp.{{ number_format((float) $x->barangs->harga_barang) }}</td>
                                             <td>{{ $x->jumlah}}</td>
-                                            <td>Rp. {{ number_format((float) $x->total) }}</td>
-                                            <td class="text-center">
-                                                <form method="post" id="delete-form-{{ $x->id }}"
-                                                    action="{{ route('delete_child', $x->id) }}"
-                                                    style="display: none">
-                                                    {{ csrf_field() }} {{ method_field('DELETE') }}
-                                                </form>
-                                                <a class="btn btn-sm btn-danger" href=""
-                                                    onclick="if(confirm('Are you sure?'))
-                                                    {
-                                                        event.preventDefault();document.getElementById('delete-form-{{ $x->id }}').submit();
-                                                    }
-                                                    else{
-                                                        event.preventDefault();
-                                                    }">Hapus
-                                                </a>
-                                            </td>
+                                            <td>Rp.{{ number_format((float) $x->total) }}</td>
+
                                         </tr>
                                     @endforeach
                                     <tr>
-                                        <td colspan="4" style="text-align: center;font-size: 12pt;font-weight: 700">
+                                        <td colspan="5" style="text-align: center;font-size: 12pt;font-weight: 700;height: 40px;">
                                             TOTAL JUMLAH
                                         </td>
                                         <td style="font-size: 15pt;font-weight: 700">
-                                            Rp. {{ number_format((float) $data['sum']) }}
+                                            Rp.{{ number_format((float) $data['sum']) }}
                                         </td>
                                     </tr>
                                 </tbody>
                             </table>
                         </div>
-                    </div>
-                    <div class="card-footer py-4">
-                        <nav class="d-flex justify-content-end" aria-label="...">
-
-                        </nav>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 </body>
 
 </html>

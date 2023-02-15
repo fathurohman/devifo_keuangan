ae2413ad574d20a2a4f450347050dfb53a8fc080
Fathurohmanoz <fathurfurs@gmail.com>
2023-02-15 11:38:06 +0700

    perbaikan tampilan print nota

--- resources/views/pdf/order_pdf.blade.php
+++ resources/views/pdf/order_pdf.blade.php
@@ -21,66 +21,79 @@
             </thead>
             <tbody>
                     <tr>
-                        <td style="text-align: center">
-                            <table style="text-align: center">
+                        <td style="width: 70%">
+                            <table>
                                 <tr>
-                                    <td colspan="3" style="text-align: center;">
-                                        <img style="width: 80%" src="{{ public_path('argon') }}/img/devifo.jpg">
+                                    <td colspan="3">
+                                        <img style="width: 60%" src="{{ public_path('argon') }}/img/notalogo.png"><br>
                                     </td>
-                                </tr>
+                                </tr><br>
                                 <tbody>
                                     <tr>
-                                        <td colspan="3" style="text-align: center;font-size: 9pt">
-                                            Jl. Cibanteng Agathis, Cihideung Ilir, Kec. Ciampea, Kabupaten Bogor, Jawa Barat 16620
+                                        <td colspan="3" style="font-size: 9pt">
+                                            Jl. Cibanteng Agathis, Cihideung Ilir, Kec. Ciampea, Kab. Bogor
                                         </td>
                                     </tr>
-                                    <tr>
-                                        <td colspan="3" style="text-align: center;font-size: 9pt">
-                                            Telp.0813-7774-7001
-                                        </td>
-                                    </tr>
+
 
                                 </tbody>
                             </table>
                         </td>
 
-                        <td style="text-align: right">
+                        <td style="text-align: right"><b>
                             <table style="text-align: right">
                                 <tr>
-                                    <td colspan="3" style="text-align: right;">Bogor, {{ Carbon\Carbon::now()->format('d F Y')}}</td>
+                                    <td style="text-align: right;">
+                                        0251-8471300 <img width="7%" src="{{ public_path('argon') }}/img/small/small_tlp.png">
+                                    </td>
                                 </tr>
-                                <tbody>
-                                    <tr>
-                                        <td colspan="3" style="text-align: right;height: 50px;">Kepada Yth.</td>
-                                    </tr>
-                                    <tr>
-                                        <td style="width: 20%">Nama</td>
-                                        <td style="width: 1%">:</td>
-                                        <td style="width: 10%"><b>{{ $data['order']->nama_pembeli}}</b></td>
-                                    </tr>
-                                    <tr>
-                                        <td>No.HP</td>
-                                        <td>:</td>
-                                        <td><b>{{ $data['order']->no_pembeli}}</b></td>
-                                    </tr>
-                                </tbody>
+                                <tr>
+                                    <td style="text-align: right;">
+                                        0813-7774-7001 <img width="7%" src="{{ public_path('argon') }}/img/small/small_wa.png">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: right;">
+                                        devifo.invitation <img width="7%" src="{{ public_path('argon') }}/img/small/small_instagram.png">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: right;">
+                                        devifo.com <img width="7%" src="{{ public_path('argon') }}/img/small/small_web.png">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: right;">
+                                        devifo.official@gmail.com <img width="7%" src="{{ public_path('argon') }}/img/small/small_gmail.png">
+                                    </td>
+                                </tr>
+                                </tr>
                             </table>
+                        </b>
                         </td>
 
                     </tr>
             </tbody>
         </table>
     </div><br>
-    NOTA No : <b>{{ $data['order']->kode_nota}}</b>
-                        <div  class="table-request">
+    <table style="width: 100%;">
+        <tr>
+            <td>
+               <b> Costumer </b>  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:  {{ $data['order']->nama_pembeli}}<br>
+    <b>No Whatsapp</b> :  {{ $data['order']->no_pembeli}}<br>
+            </td>
+            <td></td>
+            <td style="text-align: right"><b>Tanggal</b> : {{ Carbon\Carbon::now()->format('d F Y')}}</td>
+        </tr>
+    </table>
+
+                        <div class="table-request">
                             <table border="1" style="table-layout:fixed;">
                                 <thead>
-                                    <tr>
-                                        <th style="width: 5%;height: 30px;">No</th>
-                                        <th style="width: 15%;">Kode</th>
-                                        <th style="width: 35%;">Nama</th>
+                                    <tr style="background-color: #110066;color: white">
+                                        <th style="width: 15%;height: 30px;">QTY</th>
+                                        <th style="width: 35%;">Nama Barang</th>
                                         <th style="width: 20%;">Harga</th>
-                                        <th style="width: 15%;">Jumlah</th>
                                         <th style="width: 50%;">Total</th>
 
                                     </tr>
@@ -88,17 +101,22 @@
                                 <tbody>
                                     @foreach ($data['data'] as $index=>$x)
                                         <tr>
-                                            <td>{{ $index+1 }}</td>
-                                            <td style="height: 30px"> {{ $x->barangs->kode_barang }} </td>
+                                            <td style="height: 20px;">{{ $x->jumlah}}</td>
                                             <td>{{ $x->barangs->nama_barang }} </td>
                                             <td> Rp.{{ number_format((float) $x->barangs->harga_barang) }}</td>
-                                            <td>{{ $x->jumlah}}</td>
                                             <td>Rp.{{ number_format((float) $x->total) }}</td>
 
                                         </tr>
                                     @endforeach
                                     <tr>
-                                        <td colspan="5" style="text-align: center;font-size: 12pt;font-weight: 700;height: 40px;">
+                                        <td style="height: 20px;"></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+
+                                    </tr>
+                                    <tr>
+                                        <td colspan="3" style="text-align: center;font-size: 12pt;font-weight: 700;height: 40px;">
                                             TOTAL JUMLAH
                                         </td>
                                         <td style="font-size: 15pt;font-weight: 700">
@@ -120,10 +138,36 @@
                                 </thead>
                                 <tbody>
                                         <tr>
-                                            <td style="text-align: center">Tanda Terima,</td>
+                                            <td><b>
+                                            <table>
+                                                <tr>
+                                                    <td>No. Rek.</td>
+                                                    <td></td>
+                                                    <td></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>AN</td>
+                                                    <td>:</td>
+                                                    <td>AFIF RAFYAN</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>BCA</td>
+                                                    <td>:</td>
+                                                    <td>1740334723</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Mandiri</td>
+                                                    <td>:</td>
+                                                    <td>1330014784664</td>
+                                                </tr>
+                                            </table>
+                                            </b>
+                                            </td>
                                             {{-- <td style="font-size: 8pt;"><br>
                                                 <p style="text-align: left">Barang Yang Sudah Di beli tidak dapat di tukar kembali</p></td> --}}
-                                            <td style="text-align: center">Hormat Kami,</td>
+                                            <td style="text-align: center"><b>Hormat Kami,</b>
+                                            <br><br><br><br> ( &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; )
+                                            </td>
                                         </tr>
                                 </tbody>
                             </table>
